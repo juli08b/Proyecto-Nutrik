15a9ef0db3644c18030ad8a25816f9896cfdfb04
juliana <eilynjb08@gmail.com>
2026-05-05 18:13:33 -0500

    instalacion router

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/header" element={<Header />} />
+        
       </Routes>
 
       <Footer />
