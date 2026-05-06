5397e87c20ec67d0c680d6ddb31559228b444715
juliana <eilynjb08@gmail.com>
2026-05-06 12:30:38 -0500

    Arreglo del error

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,10 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Header from './Components/Header';
-<<<<<<< HEAD
 import Footer from './Components/Footer'; 
 import Login from './Pages/Login';
 import Register from './Pages/Register';
-=======
-import Footer from './Components/Footer';
 import Productos from './Pages/Product';
->>>>>>> origin/gabriel
 import './index.css';
 
 function App() {
@@ -17,7 +13,6 @@ function App() {
   const location = useLocation();
 
   // Definimos en qué rutas NO queremos que aparezca el Navbar o el Footer
-  // Por ejemplo: login y Registro (
   const ocultarLayout = location.pathname === '/login' || location.pathname === '/Registro';
 
   return (
@@ -28,15 +23,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/header" element={<Header />} />
-<<<<<<< HEAD
         <Route path="/login" element={<Login />} />
         <Route path="/Registro" element={<Register />} />
-=======
-        <Route path="/productos" element={< Productos/>} />
->>>>>>> origin/gabriel
+        <Route path="/productos" element={<Productos />} />
       </Routes>
 
-      {/* También podemos ocultar el Footer en esas páginas si quieres que el Login sea pantalla completa */}
+      {/* Solo se muestra el Footer si NO estamos en login o registro */}
       {!ocultarLayout && <Footer />}
     </>
   );
