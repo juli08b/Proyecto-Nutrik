cedff8c7210e49e78d845a144e1db921dd989472
juliana <eilynjb08@gmail.com>
2026-05-06 11:50:37 -0500

    cambios en el navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,7 @@
-import { Link } from 'react-router-dom'; // Importante para la navegación de React Router
+import { Link } from 'react-router-dom'; 
 import "./Navbar.css";
 
-// Importación del logo pequeño que querías agregar
 import logoPequeno from "../assets/logoNutrick.png"; 
-
 import tienda from "../assets/Tienda.svg";
 import nuevo from "../assets/Nuevo.svg";
 import descuento from "../assets/Descuento.svg";
@@ -18,62 +16,77 @@ function Navbar() {
     <nav className="navegacion">
       <ul className="nav-list">
         
-        {/* LOGO PEQUEÑO: Agregado al inicio como pediste */}
         <li className="nav-logo-min">
           <Link to="/">
             <img src={logoPequeno} alt="Nutrik" width={150} height={90} />
           </Link>
         </li>
 
-        {/* Enlaces principales con botones */}
         <li>
-          <button className="btn-nav">
-            <img src={categoria} alt="" width={20} height={20} />
-            <Link to="/categorias">CATEGORÍA</Link>
-          </button>
+          <Link to="/categorias" className="link-wrapper">
+            <button className="btn-nav">
+              <img src={categoria} alt="" width={20} height={20} />
+              CATEGORÍA
+            </button>
+          </Link>
         </li>
         <li>
-          <button className="btn-nav">
-            <img src={producto} alt="" width={20} height={20} />
-            <Link to="/productos">PRODUCTOS</Link>
-          </button>
+          <Link to="/productos" className="link-wrapper">
+            <button className="btn-nav">
+              <img src={producto} alt="" width={20} height={20} />
+              PRODUCTOS
+            </button>
+          </Link>
         </li>
         <li>
-          <button className="btn-nav">
-            <img src={dieta} alt="" width={20} height={20} />
-            <Link to="/dietas">DIETAS</Link>
-          </button>
+          <Link to="/dietas" className="link-wrapper">
+            <button className="btn-nav">
+              <img src={dieta} alt="" width={20} height={20} />
+              DIETAS
+            </button>
+          </Link>
         </li>
 
-        {/* Separador visual */}
         <li className="divisor-container">
           <div className="divisor"></div>
         </li>
 
-        {/* Enlaces extra a la derecha */}
         <li className="nav-extra-item">
-          <span className="icono-circulo"><img src={tienda} alt="" /></span>
-          <Link to="/tienda">Tienda</Link>
+          <Link to="/tienda" className="extra-link">
+            <span className="icono-circulo"><img src={tienda} alt="" /></span>
+            Tienda
+          </Link>
         </li>
         <li className="nav-extra-item">
-          <span className="icono-circulo"><img src={nuevo} alt="" /></span>
-          <Link to="/nuevo">Lo nuevo</Link>
+          <Link to="/nuevo" className="extra-link">
+            <span className="icono-circulo"><img src={nuevo} alt="" /></span>
+            Lo nuevo
+          </Link>
         </li>
         <li className="nav-extra-item">
-          <span className="icono-circulo"><img src={descuento} alt="" /></span>
-          <Link to="/descuentos">Descuento</Link>
+          <Link to="/descuentos" className="extra-link">
+            <span className="icono-circulo"><img src={descuento} alt="" /></span>
+            Descuento
+          </Link>
         </li>
         <li className="nav-extra-item">
-          <span className="icono-circulo"><img src={sugerencias} alt="" /></span>
-          <Link to="/sugerencias">Sugerencias</Link>
+          <Link to="/sugerencias" className="extra-link">
+            <span className="icono-circulo"><img src={sugerencias} alt="" /></span>
+            Sugerencias
+          </Link>
         </li>
+
         <li className="divisor-container">
           <div className="divisor"></div>
         </li>
+
         <li>
-          <button className="btn-nav-acceder">
-            <Link to="/Registro">ACCEDER</Link>
-          </button>
+          {/* Aquí conectamos el botón de Acceder con la página de Login */}
+          <Link to="/login" className="link-wrapper">
+            <button className="btn-nav-acceder">
+              ACCEDER
+            </button>
+          </Link>
         </li>
       </ul>
     </nav>
