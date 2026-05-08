494afe486399d2c582485f99a9ef8c7d36425e89
Alexander-Santacruz <davidalexanderchangosantacruz@gmail.com>
2026-05-08 09:08:13 -0500

    Configuración Vite: base path para producción y ajustes de servidor

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   base: mode === 'production' ? '/Proyecto-Nutrik/' : '/',
+  server: {
+    port: 5173,
+    open: true,
+    host: true,
+  },
 }));
