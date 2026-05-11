2d170a43e17190966f9286e73e73888d8a0f8980
juliana <eilynjb08@gmail.com>
2026-05-11 18:14:03 -0500

    cambios en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,32 +8,30 @@ import Register from './Pages/Register';
 import Productos from './Pages/Product';
 import ProductView from './Pages/ProductView';
 import './index.css';
-
 import './App.css';
 
 function App() {
-  // Obtenemos la ubicación actual (la URL)
   const location = useLocation();
 
-  // Definimos en qué rutas NO queremos que aparezca el Navbar o el Footer
-  const ocultarLayout = location.pathname === '/login' || location.pathname === '/Registro';
+  // Condición de Master: Solo es verdadero si estamos en la raíz (Home)
+  const esHome = location.pathname === '/';
 
   return (
     <>
-      {/* Solo se muestra el Navbar si NO estamos en login o registro */}
-      {!ocultarLayout && <Navbar />}
+      {/* Solo se muestra el Navbar si esHome es true */}
+      {esHome && <Navbar />}
 
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/header" element={<Header />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/Registro" element={<Register />} />
-      <Route path="/productos" element={<Productos />} />
-      <Route path="/ver-productos" element={<ProductView />} /> {/* <--- Esta es la nueva */}
+        <Route path="/" element={<Home />} />
+        <Route path="/header" element={<Header />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Registro" element={<Register />} />
+        <Route path="/productos" element={<Productos />} />
+        <Route path="/ver-productos" element={<ProductView />} />
       </Routes>
 
-      {/* Solo se muestra el Footer si NO estamos en login o registro */}
-      {!ocultarLayout && <Footer />}
+      {/* Solo se muestra el Footer si esHome es true */}
+      {esHome && <Footer />}
     </>
   );
 }
