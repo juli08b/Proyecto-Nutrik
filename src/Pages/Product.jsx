2c10a13d39bb9eb9be989654df360014cb78faa3
gabriel123png <gabrimh12@gmail.com>
2026-05-12 00:27:45 -0500

    avance

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -22,12 +22,12 @@ const Productos = () => {
         <div className="card-producto">
 
           <img
-            className="img-producto" src="https://i.pinimg.com/736x/4e/1d/3d/4e1d3d2831b01988bbfbcaf7fbdeabda.jpg" alt=""
+            className="img-producto" src="https://i.pinimg.com/736x/4e/1d/3d/4e1d3d2831b01988bbfbcaf7fbdeabda.jpg" alt="creatine"
           />
 
           <h3>Creatina en Polvo </h3>
 
-          <Link to="/vistaproducto/creatine">
+          <Link to="/ProductView/creatine">
              <button className="btn-producto">Ver producto</button>
           </Link>
             
@@ -38,11 +38,11 @@ const Productos = () => {
         <div className="card-producto">
 
           <img
-            className="img-producto" src="https://i.pinimg.com/1200x/04/40/75/044075ff03f913e906dca4efbafc8821.jpg" alt="" />
+            className="img-producto" src="https://i.pinimg.com/1200x/04/40/75/044075ff03f913e906dca4efbafc8821.jpg" alt="Yogurt Griego" />
 
           <h3>Yogurt Griego</h3>
 
-          <Link to="/vistaproducto/yogur">
+          <Link to="/ProductView/Yogurt Griego">
           <button className="btn-producto">Ver producto </button>
           </Link>
 
@@ -53,9 +53,9 @@ const Productos = () => {
 
           <img className="img-producto" src="https://i.pinimg.com/736x/76/0c/30/760c30841b4c4e0313ff087ef2dcc749.jpg" alt="Detox"  />
 
-          <h3>té Detox</h3>
+          <h3>Detox</h3>
 
-          <Link to="/vistaproducto">
+          <Link to="/ProductView/Detox">
           <button className="btn-producto">Ver producto </button>
           </Link>
 
@@ -63,8 +63,8 @@ const Productos = () => {
         <div className="card-producto">
           <img src= "https://i.pinimg.com/1200x/20/85/55/208555e7cad61284b90054f29ccc0fa3.jpg" alt="omega3" className="img-producto"  />
 
-          <h3>omega 3</h3>
-          <Link to="/vistaproducto">
+          <h3>omega3</h3>
+          <Link to="/ProductView/omega3">
           <button className="btn-producto">Ver producto </button>
           </Link>
 
@@ -73,58 +73,61 @@ const Productos = () => {
         <div className="card-producto">
           <img src= "https://i.pinimg.com/1200x/bf/0a/6d/bf0a6dc31db1508589493ceec6a6c512.jpg" alt="colageno" className="img-producto"  />
 
-          <h3>Colágeno Hidrolizado</h3>
+          <h3>colageno</h3>
 
-          <Link to="/vistaproducto">
+          <Link to="/ProductView/colageno">
           <button className="btn-producto">Ver producto </button>
           </Link>
 
         </div>
         <div className="card-producto">
-          <img src= "https://i.pinimg.com/736x/28/bf/0c/28bf0ce32dc25e1c1912e23c39653062.jpg" alt="omega3" className="img-producto"  />
+          <img src= "https://i.pinimg.com/736x/28/bf/0c/28bf0ce32dc25e1c1912e23c39653062.jpg" alt="chia" className="img-producto"  />
 
-          <h3>Semillas de Chía</h3>
-            <button className="btn-producto">
+          <h3>chia</h3>
+          <Link to="/ProductView/chia">
+          <button className="btn-producto">
               Ver producto
             </button>
+          </Link>
+            
 
         </div>
         <div className="card-producto">
-          <img src= "https://i.pinimg.com/736x/29/75/6f/29756f11865e9f4c61fe532cdfe48f39.jpg" alt="aceite" className="img-producto"  />
+          <img src= "https://i.pinimg.com/736x/29/75/6f/29756f11865e9f4c61fe532cdfe48f39.jpg" alt="aceite de coco" className="img-producto"  />
 
-          <h3>Aceite de Coco Natural</h3>
+          <h3>aceite de coco</h3>
 
-          <Link to="/vistaproducto">
+          <Link to="/ProductView/aceite de coco">
           <button className="btn-producto">Ver producto </button>
           </Link>
 
         </div>
         <div className="card-producto">
-          <img src= "https://i.pinimg.com/736x/fc/fa/c7/fcfac715ba3b58d061d68ecfd143b8ab.jpg" alt="aceite" className="img-producto"  />
+          <img src= "https://i.pinimg.com/736x/fc/fa/c7/fcfac715ba3b58d061d68ecfd143b8ab.jpg" alt="Maca en Polvo" className="img-producto"  />
 
           <h3>Maca en Polvo</h3>
 
-          <Link to="/vistaproducto">
+          <Link to="/ProductView/Maca en Polvo">
           <button className="btn-producto">Ver producto </button>
           </Link>
 
         </div>
          <div className="card-producto">
-          <img src= "https://i.pinimg.com/736x/5f/73/9f/5f739fea40825911401e65067a51c169.jpg" alt="aceite" className="img-producto"  />
+          <img src= "https://i.pinimg.com/736x/5f/73/9f/5f739fea40825911401e65067a51c169.jpg" alt="Granola" className="img-producto"  />
 
           <h3>Granola</h3>
 
-          <Link to="/vistaproducto">
+          <Link to="/ProductView/Granola">
           <button className="btn-producto">Ver producto </button>
           </Link>
 
         </div>
          <div className="card-producto">
-          <img src= "https://i.pinimg.com/736x/16/ae/4a/16ae4ab7cc204b95ed2cba520eee5f21.jpg" alt="aceite" className="img-producto"  />
+          <img src= "https://i.pinimg.com/736x/16/ae/4a/16ae4ab7cc204b95ed2cba520eee5f21.jpg" alt="Frutos Secos" className="img-producto"  />
 
           <h3>Frutos Secos</h3>
 
-          <Link to="/vistaproducto">
+          <Link to="/ProductView/Frutos Secos">
           <button className="btn-producto">Ver producto </button>
           </Link>
 
