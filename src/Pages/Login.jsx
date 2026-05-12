62bf8d148171892a62343a2ecb4d7709c83c50b6
juliana <eilynjb08@gmail.com>
2026-05-11 19:35:12 -0500

    Lógica y estructura de Login finalizada

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,24 +1,21 @@
-//import React from 'react';
+import { Link } from 'react-router-dom'; // Importante para la navegación
 import './Login.css';
 import logo from '../assets/logoNutrick.png'; 
-import logo2 from '../assets/comida.jpg';
 
 const Login = () => {
   return (
-    <div className="login-page"> {/* Contenedor principal de toda la pantalla */}
-      
-      {/* LADO IZQUIERDO: El panel azul con información */}
+    <div className="login-page">
       <div className="info-content">
         <img src={logo} alt="Logo Nutrik" className="login-logo" />
         <h1 className="welcome-text">Bienvenido</h1>
         <p>Lleva una vida saludable con nosotros</p>
-        </div>
+      </div>
 
-      {/* LADO DERECHO: El formulario de Login */}
       <div className="login-form-side">
         <div className="form-content">
           <h2>Bienvenido De Nuevo</h2>
-          <p>¿No tienes cuenta? <a href="#">Crea una ahora</a></p>
+          {/* Cambiamos el <a> por <Link> */}
+          <p>¿No tienes cuenta? <Link to="/Registro">Crea una ahora</Link></p>
           
           <form>
             <div className="input-field">
@@ -32,10 +29,10 @@ const Login = () => {
             <button className="btn-google"> Iniciar Sesion con Google</button>
           </form>
           
-          <p className="forgot-pass">Olvidaste tu clave? <a href="#">Click aquí</a></p>
+          <p className="forgot-pass">Olvidaste tu clave? <Link to="/forgot-password">Click aquí</Link></p>
+
         </div>
       </div>
-
     </div>
   );
 };
