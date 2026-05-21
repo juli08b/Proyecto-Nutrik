22ea0b59c7cc949d2132c4e68b20a7e34dc76823
gabriel123png <gabrimh12@gmail.com>
2026-05-21 18:04:27 -0500

    terminado

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -27,8 +27,10 @@ const Productos = () => {
 
           <h3>Creatina en Polvo </h3>
 
-          <Link to="/ProductView/creatine">
-             <button className="btn-producto">Ver producto</button>
+          <Link to="/productview/creatine">
+              <button className="btn-producto">
+                 Ver producto
+              </button>
           </Link>
             
 
@@ -38,11 +40,11 @@ const Productos = () => {
         <div className="card-producto">
 
           <img
-            className="img-producto" src="https://i.pinimg.com/1200x/04/40/75/044075ff03f913e906dca4efbafc8821.jpg" alt="Yogurt Griego" />
+            className="img-producto" src="https://i.pinimg.com/1200x/04/40/75/044075ff03f913e906dca4efbafc8821.jpg" alt="yogur" />
 
-          <h3>Yogurt Griego</h3>
+          <h3>Yogur Griego Natural</h3>
 
-          <Link to="/ProductView/Yogurt Griego">
+          <Link to="/productview/yogur">
           <button className="btn-producto">Ver producto </button>
           </Link>
 
@@ -51,11 +53,11 @@ const Productos = () => {
         {/* Producto 3 */}
         <div className="card-producto">
 
-          <img className="img-producto" src="https://i.pinimg.com/736x/76/0c/30/760c30841b4c4e0313ff087ef2dcc749.jpg" alt="Detox"  />
+          <img className="img-producto" src="https://i.pinimg.com/736x/76/0c/30/760c30841b4c4e0313ff087ef2dcc749.jpg" alt="detox"  />
 
-          <h3>Detox</h3>
+          <h3>té detox</h3>
 
-          <Link to="/ProductView/Detox">
+          <Link to="/productview/detox">
           <button className="btn-producto">Ver producto </button>
           </Link>
 
@@ -63,8 +65,8 @@ const Productos = () => {
         <div className="card-producto">
           <img src= "https://i.pinimg.com/1200x/20/85/55/208555e7cad61284b90054f29ccc0fa3.jpg" alt="omega3" className="img-producto"  />
 
-          <h3>omega3</h3>
-          <Link to="/ProductView/omega3">
+          <h3>omega 3</h3>
+          <Link to="/productview/omega">
           <button className="btn-producto">Ver producto </button>
           </Link>
 
@@ -75,21 +77,20 @@ const Productos = () => {
 
           <h3>colageno</h3>
 
-          <Link to="/ProductView/colageno">
-          <button className="btn-producto">Ver producto </button>
+          <Link to="/productview/colageno">
+            <button className="btn-producto">Ver producto </button>
           </Link>
 
         </div>
         <div className="card-producto">
           <img src= "https://i.pinimg.com/736x/28/bf/0c/28bf0ce32dc25e1c1912e23c39653062.jpg" alt="chia" className="img-producto"  />
 
-          <h3>chia</h3>
-          <Link to="/ProductView/chia">
-          <button className="btn-producto">
-              Ver producto
-            </button>
-          </Link>
-            
+          <h3>Semillas de Chía</h3>
+            <Link to="/productview/chia">
+              <button className="btn-producto">
+                Ver producto
+              </button>
+           </Link>
 
         </div>
         <div className="card-producto">
@@ -97,37 +98,37 @@ const Productos = () => {
 
           <h3>aceite de coco</h3>
 
-          <Link to="/ProductView/aceite de coco">
+          <Link to="/productview/coco">
           <button className="btn-producto">Ver producto </button>
           </Link>
 
         </div>
         <div className="card-producto">
-          <img src= "https://i.pinimg.com/736x/fc/fa/c7/fcfac715ba3b58d061d68ecfd143b8ab.jpg" alt="Maca en Polvo" className="img-producto"  />
+          <img src= "https://i.pinimg.com/736x/fc/fa/c7/fcfac715ba3b58d061d68ecfd143b8ab.jpg" alt="maca" className="img-producto"  />
 
           <h3>Maca en Polvo</h3>
 
-          <Link to="/ProductView/Maca en Polvo">
+          <Link to="/productview/maca">
           <button className="btn-producto">Ver producto </button>
           </Link>
 
         </div>
          <div className="card-producto">
-          <img src= "https://i.pinimg.com/736x/5f/73/9f/5f739fea40825911401e65067a51c169.jpg" alt="Granola" className="img-producto"  />
+          <img src= "https://i.pinimg.com/736x/5f/73/9f/5f739fea40825911401e65067a51c169.jpg" alt="granola" className="img-producto"  />
 
           <h3>Granola</h3>
 
-          <Link to="/ProductView/Granola">
+          <Link to="/productview/granola">
           <button className="btn-producto">Ver producto </button>
           </Link>
 
         </div>
          <div className="card-producto">
-          <img src= "https://i.pinimg.com/736x/16/ae/4a/16ae4ab7cc204b95ed2cba520eee5f21.jpg" alt="Frutos Secos" className="img-producto"  />
+          <img src= "https://i.pinimg.com/736x/16/ae/4a/16ae4ab7cc204b95ed2cba520eee5f21.jpg" alt="frutos" className="img-producto"  />
 
-          <h3>Frutos Secos</h3>
+          <h3>Mix de Frutos Secoss</h3>
 
-          <Link to="/ProductView/Frutos Secos">
+          <Link to="/productview/frutos">
           <button className="btn-producto">Ver producto </button>
           </Link>
 
---
+++ src/Pages/productview.jsx
@@ -0,0 +1,153 @@
+//import React from 'react'
+import './Product.css'
+
+import { useParams } from "react-router-dom";
+
+function productview() {
+
+ const { id } = useParams();
+
+   const productos = [
+
+    {
+      id: "creatine",
+      imagen: "https://i.pinimg.com/736x/4e/1d/3d/4e1d3d2831b01988bbfbcaf7fbdeabda.jpg",
+      nombre: "Creatina Monohidratada",
+      descripcion:
+        "Suplemento ideal para mejorar el rendimiento físico.",
+      precio: "$85.000",
+    },
+
+    {
+      id: "yogur",
+      imagen: "https://i.pinimg.com/1200x/04/40/75/044075ff03f913e906dca4efbafc8821.jpg",
+      nombre: "Yogur Griego Natural",
+      descripcion:
+        "Fuente natural de proteína.",
+      precio: "$12.000",
+    },
+
+    {
+      id: "detox",
+      imagen: "https://i.pinimg.com/736x/76/0c/30/760c30841b4c4e0313ff087ef2dcc749.jpg",
+      nombre: "Té Verde Detox",
+      descripcion:
+        "Bebida natural antioxidante.",
+      precio: "$18.000",
+    },
+
+    {
+      id: "omega",
+      imagen: "https://i.pinimg.com/1200x/20/85/55/208555e7cad61284b90054f29ccc0fa3.jpg",
+      nombre: "Omega 3 Fish Oil",
+      descripcion:
+        "Ayuda a la salud cardiovascular.",
+      precio: "$35.000",
+    },
+
+    {
+      id: "colageno",
+      imagen: "https://i.pinimg.com/1200x/bf/0a/6d/bf0a6dc31db1508589493ceec6a6c512.jpg",
+      nombre: "Colágeno Hidrolizado",
+      descripcion:
+        "Ayuda al cuidado de articulaciones.",
+      precio: "$79.900",
+    },
+
+    {
+      id: "chia",
+      imagen: "https://i.pinimg.com/736x/28/bf/0c/28bf0ce32dc25e1c1912e23c39653062.jpg",
+      nombre: "Semillas de Chía",
+      descripcion:
+        "Fuente natural de fibra y omega 3.",
+      precio: "$14.000",
+    },
+
+    {
+      id: "coco",
+      imagen: "https://i.pinimg.com/736x/29/75/6f/29756f11865e9f4c61fe532cdfe48f39.jpg",
+      nombre: "Aceite de Coco Natural",
+      descripcion:
+        "Ideal para cocinar y recetas saludables.",
+      precio: "$24.000",
+    },
+
+    {
+      id: "maca",
+      imagen: "https://i.pinimg.com/736x/fc/fa/c7/fcfac715ba3b58d061d68ecfd143b8ab.jpg",
+      nombre: "Maca en Polvo",
+      descripcion:
+        "Energía natural y vitalidad.",
+      precio: "$28.000",
+    },
+
+    {
+      id: "granola",
+      imagen: "https://i.pinimg.com/736x/5f/73/9f/5f739fea40825911401e65067a51c169.jpg",
+      nombre: "Granola Saludable",
+      descripcion:
+        "Mezcla nutritiva de avena y semillas.",
+      precio: "$16.000",
+    },
+
+    {
+      id: "frutos",
+      imagen: "https://i.pinimg.com/736x/16/ae/4a/16ae4ab7cc204b95ed2cba520eee5f21.jpg",
+      nombre: "Mix de Frutos Secos",
+      descripcion:
+        "Snack saludable rico en proteína.",
+      precio: "$22.000",
+    },
+
+  ];
+
+  // BUSCA EL PRODUCTO SEGÚN EL ID
+  const producto = productos.find(
+    (producto) => producto.id === id
+  );
+
+  // SI NO EXISTE
+  if (!producto) {
+    return <h1>Producto no encontrado</h1>;
+  }
+
+  return (
+
+    <div className="contenedor-vistas">
+
+      <div className="vista-producto">
+
+        <div className="imagen-producto">
+
+          <img
+            src={producto.imagen}
+            alt={producto.nombre}
+          />
+
+        </div>
+
+        <div className="info-producto">
+
+          <h1>{producto.nombre}</h1>
+
+          <p className="descripcion-producto">
+            {producto.descripcion}
+          </p>
+
+          <h2 className="precio-producto">
+            {producto.precio}
+          </h2>
+
+          <button className="btn-carrito">
+             Agregar al carrito
+          </button>
+
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default productview
