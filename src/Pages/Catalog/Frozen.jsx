7c973c9a949fd7f90665977c4730ecc5f79b2b58
juliana <eilynjb08@gmail.com>
2026-05-26 17:02:51 -0500

    cambios en una page

--- src/Pages/Catalog/Frozen.jsx
+++ src/Pages/Catalog/Frozen.jsx
@@ -1,11 +1,38 @@
-const Frozen = () => {
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import "../../App.css"; // Ruta corregida
+
+const productosFrozen = [
+  { id: 1, nombre: "Silk - almendra sin azúcar", imagen: "https://i.pinimg.com/1200x/59/7a/da/597ada481273c9ecc7b001456412b3c4.jpg", ruta: "/catalog/frozen/silk-almendra" },
+  { id: 2, nombre: "Yogurt Smoothie", imagen: "https://i.pinimg.com/736x/98/f5/df/98f5dff3ee7d44b0d7df193a046eb42e.jpg", ruta: "/catalog/frozen/yogurt-smoothie" },
+];
+
+function Frozen() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
-    <div className="frozen-container">
-      <h2>Productos Congelados</h2>
-      <p>Aquí verás toda nuestra selección de productos frescos congelados.</p>
-      {/* Aquí irá tu lógica para mostrar los productos */}
-    </div>
+    <section className="productos">
+      <div className="productos-header">
+        <h2>Congelados</h2>
+      </div>
+
+      <div className="productos-grid">
+        {productosFrozen.map((producto) => (
+          <div className="producto-card" key={producto.id}>
+            <img src={producto.imagen} alt={producto.nombre} />
+            <div className="producto-info">
+              <h3>{producto.nombre}</h3>
+              <Link to={producto.ruta}>
+                <button className="btn-ver-mas">Ver producto</button>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   );
-};
+}
 
 export default Frozen;
